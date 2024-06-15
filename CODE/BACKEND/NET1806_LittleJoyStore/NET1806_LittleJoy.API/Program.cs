6b110f83a5e989d8ec0770de8ac78fd34fcef624
lion3993vn <lion3993vn@gmail.com>
2024-06-16 00:24:02 +0700

    add: final read csv from testcase

--- CODE/BACKEND/NET1806_LittleJoyStore/NET1806_LittleJoy.API/Program.cs
+++ CODE/BACKEND/NET1806_LittleJoyStore/NET1806_LittleJoy.API/Program.cs
@@ -96,30 +96,12 @@ namespace NET1806_LittleJoy.API
 
             // ===================== FOR LOCAL DB =======================
 
-            //builder.Services.AddDbContext<LittleJoyContext>(options =>
-            //{
-            //    options.UseSqlServer(builder.Configuration.GetConnectionString("LittleJoyLocal"));
-            //});
+            builder.Services.AddDbContext<LittleJoyContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("LittleJoyLocal"));
+            });
 
             // ==========================================================
-
-            // ===================== FOR AZURE DB =======================
-
-            var connection = String.Empty;
-            if (builder.Environment.IsDevelopment())
-            {
-                connection = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
-            }
-            else
-            {
-                connection = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING");
-            }
-
-            builder.Services.AddDbContext<LittleJoyContext>(options =>
-                    options.UseSqlServer(connection));
-
-
-            // ==================== NO EDIT OR REMOVE COMMENT =======================
 
             builder.Services.AddScoped<IUserRepository, UserRepository>();
             builder.Services.AddScoped<IUserService, UserService>();
---
+++ CODE/BACKEND/NET1806_LittleJoyStore/TestProject/Ultils/DataProvider.cs
@@ -0,0 +1,20 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject.DemoTest;
+
+namespace TestProject.Ultils
+{
+    public class DataProvider
+    {
+        public static IEnumerable<T> GetTestData<T>(string filePath)
+        {
+            return CsvReaderExtensions.ReadCsvFile<T>(filePath);
+        }
+    }
+}
