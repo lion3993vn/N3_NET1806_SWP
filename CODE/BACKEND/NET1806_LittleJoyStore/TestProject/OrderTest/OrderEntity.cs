7fc1ca6d5c818b3fd0e84cc81dbe1bc9fd421c88
lion3993vn <lion3993vn@gmail.com>
2024-06-26 23:42:21 +0700

    add: test create order and get order

---
+++ CODE/BACKEND/NET1806_LittleJoyStore/TestProject/OrderTest/OrderEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.OrderTest
+{
+    public class OrderEntity
+    {
+        public int STT {  get; set; }
+        public int UserId { get; set; }
+
+        public int TotalPrice { get; set; }
+
+        public string Address { get; set; }
+
+        public string? Note { get; set; }
+
+        public int? AmountDiscount { get; set; }
+
+        public int PaymentMethod { get; set; }
+
+        public int Id { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int ResponseCode { get; set; }
+    }
+
+    public class OrderCode
+    {
+        public int Code { get; set; }
+    }
+}
